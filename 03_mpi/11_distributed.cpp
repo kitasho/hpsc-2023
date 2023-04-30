f90fe033a9e381bc1c023ab1e935098714e6c0a4
kitasho <41176959+kitasho@users.noreply.github.com>
2023-04-30 21:48:11 +0900

    assignment-mpi-and-simd

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <mpi.h>
+#include <string.h> //add for memcpy
 
 struct Body {
   double x, y, m, fx, fy;
@@ -13,7 +14,7 @@ int main(int argc, char** argv) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  Body ibody[N/size], jbody[N/size], tmp[N/size];    //add tmp
   srand48(rank);
   for(int i=0; i<N/size; i++) {
     ibody[i].x = jbody[i].x = drand48();
@@ -21,14 +22,22 @@ int main(int argc, char** argv) {
     ibody[i].m = jbody[i].m = drand48();
     ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
   }
-  int recv_from = (rank + 1) % size;
+  // int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  MPI_Win win;    //add
+  MPI_Win_create(tmp, N/size*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);    //add
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
+    // MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    
+    MPI_Win_fence(0, win);    //add
+    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);    //add
+    MPI_Win_fence(0, win);    //add
+    memcpy(&jbody, &tmp, N/size*sizeof(Body));    //add
+
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
@@ -42,12 +51,13 @@ int main(int argc, char** argv) {
     }
   }
   for(int irank=0; irank<size; irank++) {
-    MPI_Barrier(MPI_COMM_WORLD);
+    // MPI_Barrier(MPI_COMM_WORLD);
     if(irank==rank) {
       for(int i=0; i<N/size; i++) {
         printf("%d %g %g\n",i+rank*N/size,ibody[i].fx,ibody[i].fy);
       }
     }
   }
+  MPI_Win_free(&win);    //add
   MPI_Finalize();
 }
--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -1,26 +1,61 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <immintrin.h>
 
 int main() {
   const int N = 8;
   float x[N], y[N], m[N], fx[N], fy[N];
+  float j[N];  //add
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
     fx[i] = fy[i] = 0;
+    j[i] = i; //add
   }
+  __m256 jvec = _mm256_load_ps(j);
+  __m256 xvec = _mm256_load_ps(x);
+  __m256 yvec = _mm256_load_ps(y);
+  __m256 mvec = _mm256_load_ps(m);
+  __m256 mask, rxvec, ryvec, invrvec, invr3vec, fxvec, fyvec;
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m256 ivec =_mm256_set1_ps(i);
+    // if(i != j) {
+    mask = _mm256_cmp_ps(ivec,jvec,_CMP_NEQ_OQ);
+
+    // float rx = x[i] - x[j];
+    // float ry = y[i] - y[j];
+    rxvec = _mm256_sub_ps(_mm256_set1_ps(x[i]), xvec);
+    ryvec = _mm256_sub_ps(_mm256_set1_ps(y[i]), yvec);
+
+    // float r = std::sqrt(rx * rx + ry * ry);
+    invrvec = _mm256_rsqrt_ps(_mm256_add_ps(_mm256_mul_ps(rxvec,rxvec),_mm256_mul_ps(ryvec,ryvec)));
+    invr3vec = _mm256_mul_ps(invrvec,_mm256_mul_ps(invrvec,invrvec));
+
+    // fx[i] -= rx * m[j] / (r * r * r);
+    // fy[i] -= ry * m[j] / (r * r * r);
+    //blend with mask
+    fxvec = _mm256_blendv_ps(_mm256_setzero_ps(),_mm256_mul_ps(_mm256_mul_ps(rxvec,mvec),invr3vec),mask);
+    fyvec = _mm256_blendv_ps(_mm256_setzero_ps(),_mm256_mul_ps(_mm256_mul_ps(ryvec,mvec),invr3vec),mask);
+
+    __m256 tmp_x = _mm256_permute2f128_ps(fxvec,fxvec,1);
+    tmp_x = _mm256_add_ps(tmp_x,fxvec);
+    tmp_x = _mm256_hadd_ps(tmp_x,tmp_x);
+    tmp_x = _mm256_hadd_ps(tmp_x,tmp_x);
+    float fxi[N];
+    _mm256_store_ps(fxi, tmp_x);
+    fx[i] -= fxi[0];
+
+    __m256 tmp_y = _mm256_permute2f128_ps(fyvec,fyvec,1);
+    tmp_y = _mm256_add_ps(tmp_y,fyvec);
+    tmp_y = _mm256_hadd_ps(tmp_y,tmp_y);
+    tmp_y = _mm256_hadd_ps(tmp_y,tmp_y);
+    float fyi[N];
+    _mm256_store_ps(fyi, tmp_y);
+    fy[i] -= fyi[0];
+    
+    // }
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
